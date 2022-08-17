0c46c19d7735841ed728905610ef178d2fe55b7b
RicardoRA <ricardo.rito@ubidots.com>
2022-08-16 22:08:44 -0500

    :tada: [CHORE]:  Add base project settings
    - Vite config
    - tsconfig
    - libraries

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Main from '@/components/Main';
+
+const App = () => {
+  return <Main />;
+};
+
+export default App;
